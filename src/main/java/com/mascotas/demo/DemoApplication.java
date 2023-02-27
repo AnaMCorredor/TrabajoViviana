90198a16ceda903dc9fe24d1ed9126cc3b380d18
Omar-Diaz123456 <odiaztauta@gmail.com>
2023-02-27 07:30:07 -0500

    versionOmar

---
+++ src/main/java/com/mascotas/demo/DemoApplication.java
@@ -0,0 +1,40 @@
+package com.mascotas.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("\n --- BIENVENIDO A LA REGISTRADURIA PARA MASCOTAS ---");
+		Scanner op = new Scanner(System.in);
+
+		mascotas mas = new mascotas();
+		registro opcio = new registro();
+		int opcion = 0;
+
+
+
+		do{
+			System.out.println("\n MENU DE OPCIONES");
+			System.out.println("\n[1] Registro Mascotas");
+			System.out.println("\n[2] SALIR");
+			System.out.println("\nIngresar opcion: ");
+			opcion = op.nextInt();
+
+			switch(opcion){
+				case 1:
+					mas.getOpcion();
+					break;
+
+				case 2:
+					break;
+			}
+
+		}while(opcion !=2);}
+	}
+
+
